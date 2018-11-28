4d81698240bf1ea738840b15bf77785224153fbc
austinragotzy <austin.p.ragotzy@wmich.edu>
2018-11-28 14:31:52 -0500

    adding all data and docs for them as well

--- R/data.r
+++ R/data.r
@@ -131,7 +131,7 @@
 #'
 #' @format A list containing 21 rows and 1 variable
 #' \describe{
-#'   \list{x}{some variable from 4.4.3}
+#'   \item{x}{some variable from 4.4.3}
 #' }
 "ex4.4.3data"
 
@@ -141,7 +141,7 @@
 #'
 #' @format A list containing 30 rows and 1 variable
 #' \describe{
-#'   \list{x}{some variable from 7.6.3}
+#'   \item{x}{some variable from 7.6.3}
 #' }
 "ex763data"
 
@@ -151,7 +151,7 @@
 #'
 #' @format A list containing 25 rows and 1 variable
 #' \describe{
-#'   \list{x}{some variable from 6111}
+#'   \item{x}{some variable from 6111}
 #' }
 "ex6111"
 
@@ -159,10 +159,10 @@
 #'
 #' A data set containg 15 x and y values
 #'
-#' @format A list containing 15 rows and 2 variable
+#' @format A list containing 15 rows and 2 variables
 #' \describe{
-#'   \list{x}{some variable from 1053}
-#'   \list{y}{some variable frim 1053}
+#'   \item{x}{some variable from 1053}
+#'   \item{y}{some variable frim 1053}
 #' }
 "examp1053"
 
@@ -170,10 +170,10 @@
 #'
 #' A data set containg 15 x and y values
 #'
-#' @format A list containing 15 rows and 2 variable
+#' @format A list containing 15 rows and 2 variables
 #' \describe{
-#'   \list{x}{some variable from 8.3.16}
-#'   \list{y}{some variable from 8.3.16}
+#'   \item{x}{some variable from 8.3.16}
+#'   \item{y}{some variable from 8.3.16}
 #' }
 "exercise8316"
 
@@ -181,10 +181,10 @@
 #'
 #' A data set containg 30 observations of car speeds
 #'
-#' @format A list containing 30 rows and 2 variable
+#' @format A list containing 30 rows and 2 variables
 #' \describe{
-#'   \list{ind}{type of car}
-#'   \list{speed}{the speed of the car}
+#'   \item{ind}{type of car}
+#'   \item{speed}{the speed of the car}
 #' }
 "fastcars"
 
@@ -192,9 +192,236 @@
 #'
 #' A data set containg 22 observations about genexpd
 #'
-#' @format A list containing 22 rows and 2 variable
+#' @format A list containing 22 rows and 2 variables
 #' \describe{
-#'   \list{c.x..y.}{i dont know}
-#'   \list{indy}{not sure}
+#'   \item{c.x..y.}{i dont know}
+#'   \item{indy}{not sure}
 #' }
 "genexpd"
+
+#' river length
+#'
+#' A data set containg 141 observations of values of river lenght
+#'
+#' @format A list containing 141 rows and 1 variable
+#' \describe{
+#'   \item{lengthriver}{the length of rivers}
+#' }
+"lengthriver"
+
+#' life time motor
+#'
+#' A data set containg 20 observations about motor life time
+#'
+#' @format A list containing 20 rows and 1 variable
+#' \describe{
+#'   \item{lifetime}{life of a motor}
+#' }
+"lifetime"
+
+#' men 1500m
+#'
+#' A data set containg 27 observations about mens 1500m runs
+#'
+#' @format A list containing 27 rows and 2 variables
+#' \describe{
+#'   \item{year}{the year this took place}
+#'   \item{time}{the time taken to run in minutes}
+#' }
+"men1500m"
+
+#' mix668
+#'
+#' A data set containg 20 observations of x values
+#'
+#' @format A list containing 20 rows and 1 variable
+#' \describe{
+#'   \item{x}{some variable from 6.6.8}
+#' }
+"mix668"
+
+#' normal50
+#'
+#' A data set containg 50 observations of x values
+#'
+#' @format A list containing 50 rows and 1 variable
+#' \describe{
+#'   \item{x}{some x value}
+#' }
+"normal50"
+
+#' olympic 1500 marathon
+#'
+#' A data set containg 20 observations about olympic 1500 marathon
+#'
+#' @format A list containing 20 rows and 3 variables
+#' \describe{
+#'   \item{year}{the year it took place}
+#'   \item{m1500}{not sure}
+#'   \item{marathon}{not sure}
+#' }
+"olym1500mara"
+
+#' punter
+#'
+#' A data set containg 13 observations about punters
+#'
+#' @format A list containing 13 rows and 7 variables
+#' \describe{
+#'   \item{distance}{the distance in some unit}
+#'   \item{strength}{the strength in some unit}
+#'   \item{hangtime}{the time the ball was in the air}
+#'   \item{lf}{no idea}
+#'   \item{llstrength}{no idea}
+#'   \item{rf}{no idea}
+#'   \item{rl}{no idea}
+#' }
+"punter"
+
+#' quailldl
+#'
+#' A data set containg 39 observations about quailldl
+#'
+#' @format A list containing 39 rows and 2 variables
+#' \describe{
+#'   \item{treat}{i dont know}
+#'   \item{ldl}{not sure}
+#' }
+"quailldl"
+
+#' regr1
+#'
+#' A data set containg 15 observations about test scores
+#'
+#' @format A list containing 15 rows and 2 variables
+#' \describe{
+#'   \item{actscores}{scores gotten on act}
+#'   \item{examscores}{scores gotten on exams}
+#' }
+"regr1"
+
+#' scottish eye and hair color
+#'
+#' A data set containg observations about hair and eye color
+#'
+#' @format A matrix containing 4 rows of eye color and 5 columns of hair color
+#' \describe{
+#'   \item{blue}{a row of people with blue eyes}
+#'   \item{light}{a row of people with light eyes}
+#'   \item{medium}{a row of people with medium eyes}
+#'   \item{dark}{a row of people with dark eyes}
+#'   \item{fair}{a column of people with fair hair}
+#'   \item{red}{a column of people with red hair}
+#'   \item{medium}{a column of people with medium hair}
+#'   \item{dark}{a column of people with dark hair}
+#'   \item{black}{a column of people with black hair}
+#' }
+"scotteyehair"
+
+#' sec95set2
+#'
+#' A data set containg 15 observations about sec 9.5 set 2
+#'
+#' @format A list containing 15 rows and 3 variables
+#' \describe{
+#'   \item{x}{some variable x}
+#'   \item{fa}{some other variable fa}
+#'   \item{fb}{soem other variable fb}
+#' }
+"sec95set2"
+
+#' sec 9.5.1
+#'
+#' A data set containg 24 observations from section 9.5.1
+#'
+#' @format A list containing 24 rows and 3 variables
+#' \describe{
+#'   \item{x}{some variable x}
+#'   \item{fa}{some other variable fa}
+#'   \item{fb}{soem other variable fb}
+#' }
+"sec951"
+
+#' section 7.6 data
+#'
+#' A data set containg 20 observations of x values
+#'
+#' @format A list containing 20 rows and 1 variable
+#' \describe{
+#'   \item{x}{some x value}
+#' }
+"sect76data"
+
+#' self vs rival
+#'
+#' A data set containg 20 observations of self and rival
+#'
+#' @format A list containing 50 rows and 2 variables
+#' \describe{
+#'   \item{self}{what you got}
+#'   \item{rival}{what your rival got}
+#' }
+"selfrival"
+
+#' shoshoni
+#'
+#' A data set containg 20 observations about shoshoni
+#'
+#' @format A list containing 20 rows and 1 variable
+#' \describe{
+#'   \item{shoshoni}{something}
+#' }
+"shoshoni"
+
+#' speedlight
+#'
+#' A data set containg 66 observations of speeds
+#'
+#' @format A list containing 66 rows and 1 variable
+#' \describe{
+#'   \item{speed}{the speed in some units}
+#' }
+"speedlight"
+
+#' sulfur dioxide
+#'
+#' A data set containg 24 observations of sulfur dioxide levels
+#'
+#' @format A list containing 24 rows and 1 variable
+#' \describe{
+#'   \item{sulfurdioxide}{the level of sulfur dioxide}
+#' }
+"sulfurdio"
+
+#' telephone
+#'
+#' A data set containg 24 observations about phone calls
+#'
+#' @format A list containing 24 rows and 2 variables
+#' \describe{
+#'   \item{year}{the year of the observation}
+#'   \item{numcall}{the average number of calls}
+#' }
+"telephone"
+
+#' temp by gender
+#'
+#' A data set containg 130 observations of temp by gender
+#'
+#' @format A list containing 130 rows and 2 variables
+#' \describe{
+#'   \item{temp}{something}
+#'   \item{gender}{gender of the person male or female}
+#' }
+"tempbygender"
+
+#' waterwheel
+#'
+#' A data set containg 20 observations about waterwheel
+#'
+#' @format A list containing 20 rows and 2 variables
+#' \describe{
+#'   \item{numbrev}{something}
+#'   \item{indgrp2}{not sure}
+#' }
+"waterwheel"
