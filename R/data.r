f29cfcadb71d17b86edcd52b7e56cfab30f15365
austinragotzy <austin.p.ragotzy@wmich.edu>
2018-11-27 23:24:10 -0500

    first commit putting data in a package

---
+++ R/data.r
@@ -0,0 +1,200 @@
+#' aztdoses
+#'
+#' A data set containg 20 azt and doses
+#'
+#' @format A list with 20 rows and 2 variables:
+#' \describe{
+#'   \item{azt}{no clue what this means}
+#'   \item{dose}{maybe has something to do with how much given}
+#' }
+#'
+"aztdoses"
+
+#' bb
+#'
+#' A data set containing 59 with multiple types of data
+#'
+#' @format A list with 59 rows and 6 variables:
+#' \describe{
+#'   \item{hand}{right or left}
+#'   \item{height}{height in inches}
+#'   \item{weight}{weight in pounds}
+#'   \item{hitind}{something to do with hit?}
+#'   \item{hitpitind}{something}
+#'   \item{average}{some kind of an average}
+#' }
+#'
+"bb"
+
+#' beta30
+#'
+#' A data set containg 30 betas
+#'
+#' @format A list of 30 rows in 1 variable
+#' \describe{
+#'   \item{x}{a beta}
+#' }
+"beta30"
+
+#' brain data
+#'
+#' A data set containg 40 observations about human brains
+#'
+#' @format A list of 40 rows and 7 variables
+#' \describe{
+#'   \item{gender}{gender of the person male or female}
+#'   \item{FSIQ}{something}
+#'   \item{VIQ}{something}
+#'   \item{PIQ}{something}
+#'   \item{weight}{weight of the person in pounds}
+#'   \item{height}{height of the person in inches}
+#'   \item{MRI_Count}{something}
+#' }
+"braindata"
+
+#' conductivity
+#'
+#' A data set containg 18 observations about conductivity
+#'
+#' @format A list of 18 rows with 3 variables
+#' \describe{
+#'   \item{Conductivity}{the value of conductivity}
+#'   \item{Binder}{something}
+#'   \item{Aggregate}{something}
+#' }
+"conductivity"
+
+#' crimealk
+#'
+#' A data set containing the number of different crimes commited by drinkers
+#' and non drinkers
+#'
+#' @format A matrix containg 6 crimes and how many drinkers vs non drinkers
+#' commited them
+#' \describe{
+#'   \item{arson}{a row corresponding to people who commited arson}
+#'   \item{rape}{a row corresponding to people who commited rape}
+#'   \item{violence}{a row corresponding to people who commited violent crimes}
+#'   \item{theft}{a row corresponding to people who commited theft}
+#'   \item{coining}{a row corresponding to people who commited coining}
+#'   \item{fraud}{a row corresponding to people who commited fraud}
+#'   \item{drinker}{a column corresponding to the people who drink alcohol}
+#'   \item{nondrinker}{a column corresponding to the people who do not drink alcohol}
+#' }
+"crimealk"
+
+#' darwin
+#'
+#' A data set containing 15 observation from darwin
+#'
+#' @format A list containing 15 rows with 2 variables
+#' \describe{
+#'   \item{cross}{no clue}
+#'   \item{self}{no cluer}
+#' }
+"darwin"
+
+#' earthmoon
+#'
+#' A data set containing 7 ratios
+#'
+#' @format A list containg 7 rows and 1 variable
+#' \describe{
+#'   \item{ratios}{earth to moon ratios}
+#' }
+"earthmoon"
+
+#' eg 4.4.4 data
+#'
+#' A data set containing 15 values
+#'
+#' @format A list with 15 rows and 1 variable
+#' \describe{
+#'   \tiem{x}{some variable from 4.4.4}
+#' }
+"eg4.4.4data"
+
+#' elasticmod
+#'
+#' A data set containg 22 observations related to elastic
+#'
+#' @format A list with 22 rows and 2 variables
+#' \describe{
+#'   \item{elasticmod}{not sure}
+#'   \item{ind}{dont know}
+#' }
+"elasticmod"
+
+#' ex 4.4.3 data
+#'
+#' A data set containg 21 values
+#'
+#' @format A list containing 21 rows and 1 variable
+#' \describe{
+#'   \list{x}{some variable from 4.4.3}
+#' }
+"ex4.4.3data"
+
+#' ex 7.6.3 data
+#'
+#' A data set containg 30 values
+#'
+#' @format A list containing 30 rows and 1 variable
+#' \describe{
+#'   \list{x}{some variable from 7.6.3}
+#' }
+"ex763data"
+
+#' ex 6111 data
+#'
+#' A data set containg 25 values
+#'
+#' @format A list containing 25 rows and 1 variable
+#' \describe{
+#'   \list{x}{some variable from 6111}
+#' }
+"ex6111"
+
+#' ex 1053 data
+#'
+#' A data set containg 15 x and y values
+#'
+#' @format A list containing 15 rows and 2 variable
+#' \describe{
+#'   \list{x}{some variable from 1053}
+#'   \list{y}{some variable frim 1053}
+#' }
+"examp1053"
+
+#' exercise 8.3.16 data
+#'
+#' A data set containg 15 x and y values
+#'
+#' @format A list containing 15 rows and 2 variable
+#' \describe{
+#'   \list{x}{some variable from 8.3.16}
+#'   \list{y}{some variable from 8.3.16}
+#' }
+"exercise8316"
+
+#' fast cars
+#'
+#' A data set containg 30 observations of car speeds
+#'
+#' @format A list containing 30 rows and 2 variable
+#' \describe{
+#'   \list{ind}{type of car}
+#'   \list{speed}{the speed of the car}
+#' }
+"fastcars"
+
+#' genexpd
+#'
+#' A data set containg 22 observations about genexpd
+#'
+#' @format A list containing 22 rows and 2 variable
+#' \describe{
+#'   \list{c.x..y.}{i dont know}
+#'   \list{indy}{not sure}
+#' }
+"genexpd"
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(mathstat)
+
+test_check("mathstat")
