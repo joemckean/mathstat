fa9d7c3b542e9e58083f6344468030103bae0983
paulph <paul.r.phillips@wmich.edu>
2018-12-01 20:11:45 -0500

    added hierarch1

---
+++ R/hierarch1.R
@@ -0,0 +1,83 @@
+#' @title Hierarchal Bayes Model
+#'
+#' @description **NEED INFO
+#'
+#' @param nsims Number of iterations for the Gibbs sampler
+#' @param x Value observed
+#' @param tau Statistic used to measure the ordinal association between
+#' @param kstart Indicates at which value of nsims the Gibbs sample commences
+#'
+#' @details Computes the solution to Example 11.4.2 on page 681.
+#'
+#' @return **NEED INFO
+#'
+#' @seealso \code{\link{gibbser2}}
+#'
+#' @references Hogg, R. McKean, J. Craig, A. (2018) Introduction to Mathematical
+#' Statistics, 8th Ed. Boston: Pearson.
+#'
+#' @examples
+#' hierarch1(nsims = 3000, x = 6, tau = 0.05, kstart = 4000)
+#' hierarch1(nsims = 1500, x = 3, tau = 0.045, kstart = 3500)
+#'
+#' @export hierarch1
+
+hierarch1 <- function(nsims = 0,
+                      x = 0,
+                      tau = 0.05,
+                      kstart = 1) {
+    # checking arguments
+    errors <- makeAssertCollection()
+    # argument 1 nsims
+    errors$push(has_nonan(nsims, 1))
+    errors$push(has_noinf(nsims, 1))
+    errors$push(is_nonzero(nsims, 1))
+    errors$push(is_positive(nsims, 1))
+    errors$push(is_numeric(nsims, 1))
+    errors$push(is_oneelement(nsims, 1))
+    # argument 2 x
+    errors$push(has_nonan(x, 2))
+    errors$push(has_noinf(x, 2))
+    errors$push(is_nonzero(x, 2))
+    errors$push(is_positive(x, 2))
+    errors$push(is_numeric(x, 2))
+    errors$push(is_oneelement(x, 2))
+    # argument 3 tau
+    errors$push(has_nonan(tau, 3))
+    errors$push(has_noinf(tau, 3))
+    errors$push(is_nonzero(tau, 3))
+    errors$push(is_positive(tau, 3))
+    errors$push(is_numeric(tau, 3))
+    errors$push(is_oneelement(tau, 3))
+    # argument 4 kstart
+    errors$push(has_nonan(kstart, 4))
+    errors$push(has_noinf(kstart, 4))
+    errors$push(is_nonzero(kstart, 4))
+    errors$push(is_positive(kstart, 4))
+    errors$push(is_numeric(kstart, 4))
+    errors$push(is_oneelement(kstart, 4))
+    # argument check results
+    reportAssertions(errors)
+    # Function starting point
+    bold <- 1
+
+    clambda <- rep(0, (nsims + kstart))
+    cb <- rep(0, (nsims + kstart))
+
+    for(i in 1:(nsims + kstart)) {
+        clambda[i] <- rgamma(1, shape=(x + 1), scale=(bold/ (bold + 1)))
+        newy <- rgamma(1, shape=2, scale=(tau/ (clambda[i] * tau + 1)))
+
+        cb[i] <- 1/ newy
+        bold <- 1/ newy
+    }
+
+    gibbslambda <- clambda[(kstart + 1):(nsims + kstart)]
+    gibbsb <- cb[(kstart + 1):(nsims + kstart)]
+
+    # Need to know how to describe return values
+    return(list(clambda=clambda,
+                cb=cb,
+                gibbslambda=gibbslambda,
+                gibbsb=gibbsb))
+}
---
+++ tests/testthat/test_hierarch1.R
@@ -0,0 +1,112 @@
+# Test for hierarch1 function
+#
+# Returns:
+#    An error message if an tests fail
+
+context("hierarch1")
+
+test_that("edge cases", {
+    # Check for nsims
+    expect_error(hierarch1(nsims = Inf),
+                 "argument 1 cannot include an Inf or -Inf")
+    expect_error(hierarch1(nsims = -Inf),
+                 "argument 1 cannot include an Inf or -Inf")
+    expect_error(hierarch1(nsims = -1),
+                 "argument 1 must be positive")
+    # Check for x
+    expect_error(hierarch1(nsims = 100, x = Inf),
+                 "argument 2 cannot include an Inf or -Inf")
+    expect_error(hierarch1(nsims = 100, x = -Inf),
+                 "argument 2 cannot include an Inf or -Inf")
+    expect_error(hierarch1(nsims = 100, x = -1),
+                 "argument 2 must be positive")
+    # Check for tau
+    expect_error(hierarch1(nsims = 100, x = 1, tau = Inf),
+                 "argument 3 cannot include an Inf or -Inf")
+    expect_error(hierarch1(nsims = 100, x = 1, tau = -Inf),
+                 "argument 3 cannot include an Inf or -Inf")
+    expect_error(hierarch1(nsims = 100, x = 1, tau = -1),
+                 "argument 3 must be positive")
+    # Check for kstart
+    expect_error(hierarch1(nsims = 100, x = 1, kstart = Inf),
+                 "argument 4 cannot include an Inf or -Inf")
+    expect_error(hierarch1(nsims = 100, x = 1, kstart = -Inf),
+                 "argument 4 cannot include an Inf or -Inf")
+    expect_error(hierarch1(nsims = 100, x = 1, kstart = -1),
+                 "argument 4 must be positive")
+})
+
+test_that("input", {
+    # Check for nsims
+    expect_error(hierarch1(nsims = NA),
+                 "argument 1 must be numeric and non-zero")
+    expect_error(hierarch1(nsims = NA),
+                 "argument 1 must be a number")
+    expect_error(hierarch1(nsims = "test"),
+                 "argument 1 must be numeric and non-zero")
+    expect_error(hierarch1(nsims = "test"),
+                 "argument 1 must be positive")
+    expect_error(hierarch1(nsims = "test"),
+                 "argument 1 must be a number")
+    # Check for x
+    expect_error(hierarch1(nsims = 50, x = NA),
+                 "argument 2 must be numeric and non-zero")
+    expect_error(hierarch1(nsims = 50, x = NA),
+                 "argument 2 must be positive")
+    expect_error(hierarch1(nsims = 50, x = NA),
+                 "argument 2 must be a number")
+    expect_error(hierarch1(nsims = 50, x = "test"),
+                 "argument 2 must be numeric and non-zero")
+    expect_error(hierarch1(nsims = 50, x = "test"),
+                "argument 2 must be positive")
+    expect_error(hierarch1(nsims = 50, x = "test"),
+                 "argument 2 must be a number")
+    # Check for tau
+    expect_error(hierarch1(nsims = 50, x = 1, tau = NA),
+                 "argument 3 must be numeric and non-zero")
+    expect_error(hierarch1(nsims = 50, x = 1, tau = NA),
+                 "argument 3 must be positive")
+    expect_error(hierarch1(nsims = 50, x = 1, tau = NA),
+                 "argument 3 must be a number")
+    expect_error(hierarch1(nsims = 50, x = 1, tau = "test"),
+                 "argument 3 must be numeric and non-zero")
+    expect_error(hierarch1(nsims = 50, x = 1, tau = "test"),
+                 "argument 3 must be positive")
+    expect_error(hierarch1(nsims = 50, x = 1, tau = "test"),
+                 "argument 3 must be a number")
+    # Check for kstart
+    expect_error(hierarch1(nsims = 50, x = 1, kstart = NA),
+                 "argument 4 must be numeric and non-zero")
+    expect_error(hierarch1(nsims = 50, x = 1, kstart = NA),
+                 "argument 4 must be positive")
+    expect_error(hierarch1(nsims = 50, x = 1, kstart = NA),
+                 "argument 4 must be a number")
+    expect_error(hierarch1(nsims = 50, x = 1, kstart = "test"),
+                 "argument 4 must be numeric and non-zero")
+    expect_error(hierarch1(nsims = 50, x = 1, kstart = "test"),
+                 "argument 4 must be positive")
+    expect_error(hierarch1(nsims = 50, x = 1, kstart = "test"),
+                 "argument 4 must be a number")
+})
+
+test_that("output", {
+    # Check class output name
+    expect_equal(attributes(hierarch1(nsims = 3000, x = 6, tau = 0.05, kstart = 4000))$names,
+                 c("clambda", "cb", "gibbslambda", "gibbsb"))
+    # Check class output is.numeric
+    expect_true(is.numeric(hierarch1(nsims = 3000, x = 6, tau = 0.05, kstart = 4000)$clambda))
+    expect_true(is.numeric(hierarch1(nsims = 3000, x = 6, tau = 0.05, kstart = 4000)$cb))
+    expect_true(is.numeric(hierarch1(nsims = 3000, x = 6, tau = 0.05, kstart = 4000)$gibbslambda))
+    expect_true(is.numeric(hierarch1(nsims = 3000, x = 6, tau = 0.05, kstart = 4000)$gibbsb))
+    # Check class output length
+    expect_equal(length(hierarch1(nsims = 3000, x = 6, tau = 0.05, kstart = 4000)$clambda),
+                 7000)
+    expect_equal(length(hierarch1(nsims = 3000, x = 6, tau = 0.05, kstart = 4000)$cb),
+                7000)
+    expect_equal(length(hierarch1(nsims = 3000, x = 6, tau = 0.05, kstart = 4000)$gibbslambda),
+                 3000)
+    expect_equal(length(hierarch1(nsims = 3000, x = 6, tau = 0.05, kstart = 4000)$gibbsb),
+                 3000)
+})
+
+
