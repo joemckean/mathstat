bc626d6eb093fa81ccc751d10f6827d1961edf37
paulph <paul.r.phillips@wmich.edu>
2018-12-05 16:00:21 -0500

    updated onesampsgn documentation

--- R/onesampsgn.R
+++ R/onesampsgn.R
@@ -5,8 +5,11 @@
 #' variable.
 #' .
 #' @param x Sample of size 'n'
-#' @param test Need Info
-#' @param alt Need Info
+#' @param test Computes a two-sided sign test about theta0. If \code{test} is true,
+#'  then H_0: theta = theta0 vs. H_1: theta != theta0 is conducted.
+#' @param alt Computes a one-sided sign test for theta0. If \code{alt} equals 1,
+#' then the test H_1: theta > theta0 is conducted. If \code{alt} equals -1, then
+#' H_1: theta < theta0 is conducted.
 #' @param theta0 Variance of sample \emph{x}
 #' @param alpha Level of significance
 #' @param maktable If true provides more descriptive output for x
@@ -30,9 +33,14 @@
 #' @export onesampsgn
 # Test of H_0: theta = theta0
 
-onesampsgn <- function(x, test = FALSE, alt = 0, theta0 = 0, alpha = 0.05, maktable = TRUE, plotb = FALSE) {
-  # If true a boxplot is produced
-  
+onesampsgn <- function(x,
+					   test = FALSE,
+					   alt = 0,
+					   theta0 = 0,
+					   alpha = 0.05,
+					   maktable = TRUE,
+					   plotb = FALSE) {
+
   # checking arguments
   errors <- makeAssertCollection()
   # argument 1 x
@@ -67,26 +75,26 @@ onesampsgn <- function(x, test = FALSE, alt = 0, theta0 = 0, alpha = 0.05, makta
   errors$push(is_logical(plotb, 7))
   # argument check results
   reportAssertions(errors)
-  
+
   # Check for alpha
   if (alpha < 0 || alpha >= 1) {
     stop(gettext("input argument 'alpha' must be between zero and one"))
   }
-  
+
   # Function starting point
   n <- length(x)
-  
+
   ind <- rep(0, n)
   ind[x == theta0] <- 1
-  
+
   n <- n - sum(ind)
-  
+
   if (test) {
     ts <- sum(sign(x - theta0))
-    
+
     zp <- (ts - 1)/sqrt(n)
     zn <- (ts + 1)/sqrt(n)
-    
+
     if (alt == 1) {
       pval <- 1 - pnorm(zp)
       zs <- zp
@@ -96,7 +104,7 @@ onesampsgn <- function(x, test = FALSE, alt = 0, theta0 = 0, alpha = 0.05, makta
     } else if (alt == 0) {
       if (ts >= 0) {
         pval <- 2 * (1 - pnorm(zp))
-        
+
         zs <- zp
       } else {
         pval <- 2 * pnorm(zn)
@@ -104,69 +112,69 @@ onesampsgn <- function(x, test = FALSE, alt = 0, theta0 = 0, alpha = 0.05, makta
       }
     }
   }
-  
+
   # Estimation
   n <- length(x)
   est <- median(x)
-  
+
   xs <- sort(x)
-  
+
   crit <- -qnorm(alpha/2)
-  
+
   cut <- round((n/2) - crit * sqrt(n/4) - 0.5)
-  
+
   if (cut < 0) {
     cut <- 0
   }
-  
+
   lci <- xs[cut + 1]
   uci <- xs[n - cut]
-  
+
   acconf <- 1 - 2 * pbinom(cut, n, 1/2)
-  
+
   tau <- (sqrt(n) * (uci - lci))/(2 * crit)
-  
+
   if (maktable) {
     if (test) {
       cat("\n")
       cat("Results for the Sign procedure", "\n")
-      
+
       if (alt == 0) {
         cat("Test of theta =", theta0, " versus theta not equal to ", theta0, "\n")
       }
-      
+
       if (alt == 1) {
         cat("Test of theta =", theta0, " versus theta greater than ", theta0, "\n")
       }
-      
+
       if (alt == -1) {
         cat("Test of theta =", theta0, " versus theta less than ", theta0, "\n")
       }
-      
+
       cat("Test stat. S is", ts, " Standardized (z) Test-Stat.", zs, "p-value", pval, "\n")
-      
+
       cat("\n")
     }
-    
+
     cat("Estimate ", est, " SE is ", tau/sqrt(n), "\n")
-    
+
     pct <- 100 * (1 - alpha)
-    
+
     cat(pct, "%", "Confidence Interval is ", "(", lci, ",", uci, ")", "\n")
-    
+
     cat("    Actual Confidence  is ", acconf, "\n")
-    
+
     cat("Estimate of the scale parameter tau", tau, "\n")
     cat("\n", "This CI is the asymptotic form, see Hettmansperger and McKean (2011)", "\n")
   }
-  
+
   if (plotb) {
     boxplot(x, notch = TRUE)
     title(main = "95% Notched Boxplot of Data")
   }
-  
+
   if (test == TRUE) {
-    return(list(ts = ts, zs = zs, pval = pval, est = est, lci = lci, uci = uci, acconf = acconf, 
+    return(list(ts = ts, zs = zs, pval = pval, est = est, lci = lci, uci = uci, acconf = acconf,
       tau = tau))
   } else {
     return(list(est = est, lci = lci, uci = uci, acconf = acconf, tau = tau))
